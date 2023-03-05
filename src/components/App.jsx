4421b6ed11e06fd1dc579305e70cd1c8ad53173e
Alex-byte256 <oleksandr.kozak.2019@gmail.com>
2023-03-05 18:57:56 +0300

    add fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,23 +10,32 @@ export class App extends Component{
     hits: [],
     page:1,
     name: '',
-    loading: false
+    loading: false,
+    totalHits: 0,
   }
 
   handleOnClick =()=>{
-    this.setState({page: this.state.page + 1});
-    this.handleSubmit(this.state.name);
+    this.setState(prevState =>{
+      return{
+        ...prevState,
+        page: prevState.page + 1
+      }
+    });
+    const page = this.state.page + 1;
+    this.handleSubmit(this.state.name,page)
   }
 
-  handleSubmit =(name) => {
+  handleSubmit =(name,page) => {
+     let curentPage = page || this.state.page;
     if(name !== this.state.name){
       this.setState({hits:[],page:1})
+      curentPage = 1;
     }
     this.setState({name,loading:true})
-    fetch(`https://pixabay.com/api/?q=${name}&page=${this.state.page}&key=32190831-932b1a3f6204f940916e3fe08&image_type=photo&orientation=horizontal&per_page=12`)
+    fetch(`https://pixabay.com/api/?q=${name}&page=${curentPage}&key=32190831-932b1a3f6204f940916e3fe08&image_type=photo&orientation=horizontal&per_page=12`)
       .then(el => el.json()).then(el => {
         if(el.hits){
-          this.setState({hits: [...this.state.hits,...el.hits],loading:false})
+          this.setState({hits: [...this.state.hits,...el.hits],loading:false,totalHits:el.totalHits})
         }
       })
   }
@@ -35,7 +44,7 @@ render() {
     <div className="App">
       <Searchbar onSubmit={this.handleSubmit}/>
       <ImageGallery hits={this.state.hits}/>
-      {this.state.hits.length?<Button handleOnClick={this.handleOnClick}/>:""}
+      {this.state.hits.length < this.state.totalHits?<Button handleOnClick={this.handleOnClick}/>:""}
       {this.state.loading?<Loader/>:''}
     </div>
   );
